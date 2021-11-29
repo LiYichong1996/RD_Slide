ae1f4a205ab6b387666ad31aaf40e1334ccf0aa8
Dictatura <dictatura@yeah.net>
2021-11-29 10:06:18 +0800

    增加类函数 get_index_ep, 返回当前的图的动作位点, place=init_l+ep

--- rl_lib/environment.py
+++ rl_lib/environment.py
@@ -30,12 +30,14 @@ class Env_RNA(gym.Env):
         self.action_space = action_space
         self.h_weight = h_weight
         self.id_list = list(range(len(dotB_list)))
+        self.len_list = []
 
     def reset(self, init_len=1):
         gen_work = partial(get_graph, h_weight=self.h_weight)
         self.graphs = self.pool.map(gen_work, self.dotB_list)
         init_work = partial(init_graph, init_len=init_len, action_space=self.action_space)
         self.graphs = self.pool.map(init_work, self.graphs)
+        self.len_list = [len(graph.y['seq']) for graph in self.graphs]
 
         return torch_geometric.data.Batch.from_data_list(self.graphs).clone()
 
@@ -70,6 +72,12 @@ class Env_RNA(gym.Env):
         self.graphs = [self.graphs[i] for i in range(len(self.graphs)) if i not in finish_index]
         self.id_list = [self.id_list[i] for i in range(len(self.id_list)) if i not in finish_index]
         return remove_id_list, remove_graph_list
+
+    def get_index_ep(self, place):
+        len_list = [1] + self.len_list[:-1]
+        index_list = [len_list[i]+place-1 for i in range(len(self.len_list))]
+        index = torch.tensor(index_list)
+        return index
 
 
 
