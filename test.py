8c1c4f9499041c3dcc4a76dfa8d9fc64389966d6
Dictatura <dictatura@yeah.net>
2021-12-01 10:26:04 +0800

    test only

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import RNA
 import numpy as np
+import torch
 
 from utils.rna_lib import structure_dotB2Edge, structure_edge2DotB
 
-dotB = '(((((((....(((...........)))((((((((..(((((((((((((((((((...(((((......))))).)))))).)))))))))))))..))))))))..)))))))'
-edge_index = structure_dotB2Edge(dotB)
-dotB_ = structure_edge2DotB(edge_index)
+t = torch.Tensor([0,0,0,1])
 
-dist = RNA.hamming_distance(dotB_, dotB)
+flag = torch.any(t == 1.)
 
-print(dist)
+print(flag)
